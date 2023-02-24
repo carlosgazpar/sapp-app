f559a1c2d24c179d40d883cd96fe4240112ce918
Juan Carlos Barrios <tornillosbabe@hotmail.com>
2023-02-23 23:36:03 -0600

    finalizacion de links y loader

---
+++ scripts/loader.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', () => {
+    const dots = document.getElementById('dots')
+    dots.classList.add('dots-hide')
+
+    const sectionLeft = document.querySelector('.section-left')
+    sectionLeft.classList.add('section-left-hide')
+
+    const sectionRight = document.querySelector('.section-right')
+    sectionRight.classList.add('section-right-hide')
+})
